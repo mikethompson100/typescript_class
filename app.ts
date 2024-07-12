24477a8688b15d2bed1c4eacc411851f20a4b910
Michael Thompson <michael.a.thompson@capgemini.com>
2024-07-12 10:39:31 -0400

    Finished lesson on Control Flow type analysis.

--- app.ts
+++ app.ts
@@ -1,3 +1,20 @@
-let greeting: string = 'Hello TypeScript with new files!!!';
+function GetReview(title: string): string | number {
+    if (title === 'A New Hope') {
+        return 'An instant classic!';
+    } else {
+        return Math.floor(Math.random() * 10);
+    }
+}
 
-console.log(greeting);
+let movieTitle: string = 'A New Hope';
+
+let movieReview: string | number = GetReview(movieTitle);
+
+console.log(`Movie title: ${movieTitle}`);
+
+if (typeof(movieReview) == 'string') {
+    console.log(`Review: ${movieReview}`);
+}
+else {
+    console.log(`Rating: ${movieReview}/10`);
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 "use strict";
-let greeting = 'Hello TypeScript with new files!!!';
-console.log(greeting);
+function GetReview(title) {
+    if (title === 'A New Hope') {
+        return 'An instant classic!';
+    }
+    else {
+        return Math.floor(Math.random() * 10);
+    }
+}
+let movieTitle = 'A New Hopee';
+let movieReview = GetReview(movieTitle);
+console.log(`Movie title: ${movieTitle}`);
+if (typeof (movieReview) == 'string') {
+    console.log(`Review: ${movieReview}`);
+}
+else {
+    console.log(`Rating: ${movieReview}/10`);
+}

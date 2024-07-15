58031b37c0e0c144c022210a5d01c13f2ac3e1fb
Michael Thompson <michael.a.thompson@capgemini.com>
2024-07-15 13:08:49 -0400

    Starting Annotating Functions lesson.

--- app.ts
+++ app.ts
@@ -5,16 +5,3 @@ function GetReview(title: string): string | number {
         return Math.floor(Math.random() * 10);
     }
 }
-
-let movieTitle: string = 'A New Hope';
-
-let movieReview: string | number = GetReview(movieTitle);
-
-console.log(`Movie title: ${movieTitle}`);
-
-if (typeof(movieReview) == 'string') {
-    console.log(`Review: ${movieReview}`);
-}
-else {
-    console.log(`Rating: ${movieReview}/10`);
-}
